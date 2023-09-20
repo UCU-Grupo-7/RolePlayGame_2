c50b77ae66e1b1b0c5d93049a17139273e3587ec
nanosanchez22 <nanosanchez22@gmail.com>
2023-09-20 09:51:06 -0300

    reference y test

--- test/Test.Library/UnitTest1.cs
+++ test/Test.Library/UnitTest1.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace Test.Library
+namespace RoleplayGame
 {
     public class Tests
     {
@@ -10,9 +10,27 @@ namespace Test.Library
         }
 
         [Test]
-        public void Test1()
+        public void Curar()
+        {
+            Dwarf gimli = new Dwarf("Gimli");
+            /* gimli.Axe = new Axe();
+            gimli.Helmet = new Helmet();
+            gimli.Shield = new Shield(); */
+            gilmi.Cure();
+            Assert.AreEqual(100, Dwarf.health);
+        }
+        [Test]
+        public void Test2()
         {
             Assert.Pass();
         }
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+        
     }
+
+    
 }
